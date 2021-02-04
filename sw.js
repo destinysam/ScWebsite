f7d355eacf29d36a6c238d000af975e50d1752db
destinysam <sams44802@gmail.com>
2021-02-04 14:45:31 +0530

    Modfying ServiceWorker

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
 self.addEventListener("install",e =>{
     e.waitUntil(
         Cache.open('static'),then(Cache =>{
-            return caches.addAll("./","Main.css","images/logo/192.png")
+            return caches.addAll("./","Main.css","images/logo192.png")
         })
-    )
+    );
 });
 self.addEventListener("fetch",e =>{
     e.respondWith(
