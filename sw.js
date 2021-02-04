49f3f622ef8f8a19692d792bfbd09e62925b967d
destinysam <sams44802@gmail.com>
2021-02-04 16:29:25 +0530

    Adding fetch fun

--- sw.js
+++ sw.js
@@ -5,10 +5,10 @@ self.addEventListener("install",e =>{
         })
     );
 });
-// self.addEventListener("fetch",e =>{
-//     e.respondWith(
-//         caches.match(e.request).then(response =>{
-//             return response || fetch(e.request)
-//         })
-//     );    
-// });
+self.addEventListener("fetch",e =>{
+    e.respondWith(
+        caches.match(e.request).then(response =>{
+            return response || fetch(e.request)
+        })
+    );    
+});
